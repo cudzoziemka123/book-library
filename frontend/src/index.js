30091c532f2c20601b3a000b4d1ef010d4716013
Alisa Borkowska <alisa.borkowska@gmail.com>
2025-03-15 22:16:03 +0100

    initialize redux add and display books

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import store from './app/store'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom/client'
+
 import './index.css'
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<App />)
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
