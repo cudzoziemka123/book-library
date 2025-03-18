4218eaab0f82ea18cfe0da360df6b6a718093caf
Alisa Borkowska <alisa.borkowska@gmail.com>
2025-03-18 17:14:55 +0100

    ErrorSlice added

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import booksReducer from './slices/BooksSlice'
 import filterReducer from './slices/FilterSlice'
+import errorReducer from './slices/ErrorSlice'
 
 export default configureStore({
   reducer: {
     books: booksReducer,
     filter: filterReducer,
+    error: errorReducer,
   },
 })
